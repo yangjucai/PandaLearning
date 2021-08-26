bba0840f71b19ee43dea4529eb0a320c350f8999
yangjucai <1165456797@qq.com>
2021-08-27 01:56:58 +0800

    fix login on server

--- PandaLearning/LoginViewController.swift
+++ PandaLearning/LoginViewController.swift
@@ -28,73 +28,72 @@ class LoginViewController: UIViewController {
     @IBAction func LoginPressed(_ sender: UIButton) {
 //        let loginVC = LoginController()
 //        present(loginVC, animated: true, completion: nil)
-        
+       sendLoginRequest()
+       
+    }
+    
+    @objc func sendLoginRequest() {
         let username = email.text!   //获取用户名
         let password = password.text!    //获取密码
-       
-
-
-        let credentials = JSON(dictionaryLiteral: ("email",username),("password", password))
-
-        /// Connect to localhost through an URLSession
-//        var loginRequest = URLRequest(url: URL(string: "http://192.168.50.19/login.php")!)
-        var loginRequest = URLRequest(url: URL(string: "http://1.117.107.95:8000/login/")!)
-        loginRequest.httpMethod = "POST"
-        loginRequest.httpBody = credentials.description.data(using: .utf8)
-
-        let task = URLSession.shared.dataTask(with: loginRequest) { (data, response, error) in
-            if let e = error {
-                print(e)
-                return
-            }
-
-            do {
-                let raw = try JSON(data: data!)
-                print(raw)
-                DispatchQueue.main.async {
-//                    self.homeViewController?.loginButton.setTitle("Log out", for: .normal)
-//                    self.homeViewController?.loginButton.tintColor = redTint
-//                    self.homeViewController?.loginButton.layer.borderColor = redTint.cgColor
-//                    self.dismissView()
-                    let mainBoard:UIStoryboard! = UIStoryboard(name: "Main", bundle: nil)
-                    let VCMain = mainBoard!.instantiateViewController(identifier: "vcMain")
-                    UIApplication.shared.windows[0].rootViewController = VCMain
+        
+        let urlString:String = "http://1.117.107.95:8000/login/" // 请求地址
+                let requestUrl = URL.init(string:urlString)
+                var urlRequest = URLRequest.init(url: requestUrl!)
+                urlRequest.httpMethod = "POST"
+                let configuration:URLSessionConfiguration = URLSessionConfiguration.default
+                let manager = URLSession(configuration: configuration)
+                        // 设置表单分隔符
+                let boundary = "Boundary-\(UUID().uuidString)"
+                urlRequest.setValue("multipart/form-data; boundary=\(boundary)",
+                    forHTTPHeaderField:"Content-Type")
+                var bodyData = Data()
+        let paramers = ["email": username, "password": password ]
+                for(key, value) in paramers {
+                    // 数据之前要用 --分隔线 来隔开 ，否则后台会解析失败
+                    bodyData.append("--\(boundary)\r\n".data(using:String.Encoding.utf8)!)
+                    bodyData.append("--\(boundary)\r\n".data(using:String.Encoding.utf8)!)
+                    bodyData.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: String.Encoding.utf8)!)
+                    bodyData.append("\(value)\r\n".data(using:String.Encoding.utf8)!)
                 }
-
-                print("login success")
-//                let mainBoard:UIStoryboard! = UIStoryboard(name: "Main", bundle: nil)
-//                let VCMain = mainBoard!.instantiateViewController(identifier: "vcMain")
-//                UIApplication.shared.windows[0].rootViewController = VCMain
-
-
-            } catch {
-                guard let msg = String(data: data!, encoding: .utf8) else {
-                    print("Unable to parse data string.")
-                    return
+                // --分隔线-- 为整个表单的结束符
+                bodyData.append("--\(boundary)--\r\n".data(using:String.Encoding.utf8)!)
+                urlRequest.httpBody = bodyData
+//                LRLog(message:"Request URL \(_URLStr) paramers:\(paramers!)")
+                let task = manager.dataTask(with: urlRequest){(data,response,error)in
+                    if let e = error {
+                        print(e)
+                        return
+                    }
+                    
+                    guard let msg = String(data: data!, encoding: .utf8) else {
+                        print("Unable to parse data string.")
+                        return
+                    }
+    
+                    if(msg == "登陆成功" ){
+                        DispatchQueue.main.async {
+        
+                            let mainBoard:UIStoryboard! = UIStoryboard(name: "Main", bundle: nil)
+                            let VCMain = mainBoard!.instantiateViewController(identifier: "vcMain")
+                            UIApplication.shared.windows[0].rootViewController = VCMain
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            // If user can't login, display the error message from the server.
+                            let alert = UIAlertController(title: "Login Error", message: msg, preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                        }
+                    }
                 }
-                DispatchQueue.main.async {
-                    // If user can't login, display the error message from the server.
-                    let alert = UIAlertController(title: "Login Error", message: msg, preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
-                }
-            }
-        }
-        task.resume()
+                task.resume()
+        
     }
     
     @IBAction func RegisterPressed(_ sender: UIButton) {
-        let safariVC = SFSafariViewController(url: URL(string: "http://110.40.185.37/")!)
+        let safariVC = SFSafariViewController(url: URL(string: "http://110.40.185.37/M_signup")!)
         present(safariVC, animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
